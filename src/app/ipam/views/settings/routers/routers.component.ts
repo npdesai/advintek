409cfdfddc19655c06ceae12f1a87ac8ec5eacc8
smvora4u <smvora.4u@gmail.com>
2021-10-26 19:49:57 +0530

    Delete router implemented on client side.

--- src/app/ipam/views/settings/routers/routers.component.ts
+++ src/app/ipam/views/settings/routers/routers.component.ts
@@ -9,6 +9,7 @@ import { RouterService } from 'src/app/ipam/services/router.service';
 })
 export class RoutersComponent implements OnInit {
   routers: Router[] = [];
+  selectedRouters: Router[] = [];
   statusMessage: string = '';
   width = 100;
 
@@ -24,6 +25,27 @@ export class RoutersComponent implements OnInit {
 
   closeAddDevice(width) {
     this.width = width;
+  }
+
+  onDelete() {
+    if(this.selectedRouters.length > 0) {
+      this.routers = this.routers.filter((router) => {
+        return this.selectedRouters.indexOf(router) < 0;
+      });
+      this.selectedRouters.splice(0, this.selectedRouters.length);
+    } else {
+      alert('Please Select atleast 1 Router!');
+    }
+  }
+
+  routerRowChkBoxHandler(e, router) {
+    if(e.target.checked) {
+      this.selectedRouters.push(this.routers.find((router) => {
+        return router.deviceId === e.target.value;
+      }));
+    } else {
+      this.selectedRouters.splice(this.selectedRouters.indexOf(router), 1);
+    }
   }
 
   getRouterData() {
